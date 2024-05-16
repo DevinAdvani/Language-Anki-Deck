63c1408c048414de3b0f5434ecd387ccf3aae6c1
DevinAdvani <advani.devin@gmail.com>
2024-05-17 00:04:51 +0100

    Basic Anki Deck Creation

---
+++ anki.py
@@ -0,0 +1,34 @@
+import genanki 
+
+class anki:
+    def __init__(self, name):
+        self.name = name
+
+    def make_deck(self):
+
+        my_model = genanki.Model(
+            1607392319,
+            'Simple Model',
+            fields=[
+                {'name': 'Question'},
+                {'name': 'Answer'},
+            ],
+            templates=[
+                {
+                'name': 'Card 1',
+                'qfmt': '{{Question}}',
+                'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}',
+                },
+            ])
+        
+        my_note = genanki.Note(
+            model=my_model,
+            fields=['Capital of Argentina', 'Buenos Aires'])
+        
+        my_deck = genanki.Deck(
+        2059400110,
+        'Country Capitals')
+
+        my_deck.add_note(my_note)
+
+        genanki.Package(my_deck).write_to_file('output.apkg')
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import audio
+import anki
 
+"""
 my_audio = audio.audio("frequency.txt")
 my_audio.make_list()
-#my_audio.make_audio()
+my_audio.make_audio()
 my_audio.delete_audio()
+"""
+
+my_anki = anki.anki("idk")
+my_anki.make_deck()
