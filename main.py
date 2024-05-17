82cedd135cd17ea8c32fed195513cf5829cae4d7
DevinAdvani <advani.devin@gmail.com>
2024-05-17 12:12:24 +0100

    I think this project is finished for now as can generate small decks of french pronunciations

--- main.py
+++ main.py
@@ -2,58 +2,50 @@ from gtts import gTTS
 import os
 import genanki
 
+start = 100
+number_of_words = 100
+end = start + number_of_words
 
 my_file = open("frequency.txt", "r", encoding = "utf-8")
 data = my_file.read()
 list = data.split("\n")
 
-for i in range(0,5):#len(self.word_list)):
+for i in range(start,end):
     output = gTTS(text = list[i], lang = "fr")
     output.save(list[i] + ".mp3")
 
-
-
-
 my_deck = genanki.Deck(
   2059400110,
-  'Country Capitals')
+  "French Words::" + str(start) + " - " + str(end))
 
 my_model = genanki.Model(
   1091735104,
-  'Simple Model with Media',
+  "French Words::" + str(start) + " - " + str(end),
   fields=[
-    {'name': 'Question'},
-    {'name': 'MyMedia'},                                  # ADD THIS
+    {'name': 'Word'},
+    {'name': 'Audio'},
   ],
   templates=[
     {
       'name': 'Card 1',
-      'qfmt': '{{Question}}',              # AND THIS
-      'afmt': '{{MyMedia}}',
+      'qfmt': '{{Word}}',
+      'afmt': '{{Audio}}',
     },
   ])
-
-my_note = genanki.Note(
-  model=my_model,
-  fields=['Capital of Argentina', '[sound:et.mp3]'])
-
-
-
-my_deck.add_note(my_note)
 my_package = genanki.Package(my_deck)
-my_package.media_files = ['et.mp3']
+my_package.media_files = []
 
+for i in range(start,end):
+    my_note = genanki.Note(
+    model=my_model,
+    fields=[list[i], '[sound:' + list[i] + '.mp3]'])
 
-my_package.write_to_file('output.apkg')
+    my_deck.add_note(my_note)
+    my_package.media_files.append(list[i] + ".mp3")
 
+filename = str(start) + "-" + str(end) + ".apkg"
 
+my_package.write_to_file(filename)
 
-
-
-
-
-
-
-
-#for i in range(0,5):
-#    os.remove(list[i] + ".mp3")
+for i in range(start,end):
+    os.remove(list[i] + ".mp3")
