2262211f77ccc5c49a98539d37e1a1adc8ef2a26
DevinAdvani <advani.devin@gmail.com>
2024-09-20 16:05:20 +0100

    Made File creation easy

--- make_files.py
+++ make_files.py
@@ -5,9 +5,15 @@ from main import *
 
 folder_name = input("FOLDER NAME: ")
 file_number = input("FILE AMOUNT: ")
+edited_folder_name = ""
+for i in range(0,len(folder_name)):    
+    if (folder_name[i] == " "):
+        edited_folder_name += '_'
+    else:
+        edited_folder_name += folder_name[i]
 try:
-    os.mkdir(folder_name)
+    os.mkdir(edited_folder_name)
 except:
     print("FILE ALREADY EXISTS")
 for i in range(1,int(file_number) + 1):
-    os.system("echo " + str(i) + " > " + folder_name + "/" + str(i) + ".txt")
+    os.system("echo " + str(i) + " > " + edited_folder_name + "/Paragraph_" + str(i) + ".txt")
