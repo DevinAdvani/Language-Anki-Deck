c18a6c913c7793645dd11c1f0c54edc03e68e565
DevinAdvani <advani.devin@gmail.com>
2024-09-23 16:03:47 +0100

    Making files

--- make_files.py
+++ make_files.py
@@ -4,16 +4,15 @@ from main import *
 #Make it so that multiword filenames work
 
 folder_name = input("FOLDER NAME: ")
-file_number = input("FILE AMOUNT: ")
+file_number_start = input("FILE NUMBER START: ")
+file_number_end = input("FILE NUMBER END: ")
+
 edited_folder_name = ""
 for i in range(0,len(folder_name)):    
     if (folder_name[i] == " "):
         edited_folder_name += '_'
     else:
         edited_folder_name += folder_name[i]
-try:
-    os.mkdir(edited_folder_name)
-except:
-    print("FILE ALREADY EXISTS")
-for i in range(1,int(file_number) + 1):
+
+for i in range(int(file_number_start), int(file_number_end) + 1):
     os.system("echo " + str(i) + " > " + edited_folder_name + "/Paragraph_" + str(i) + ".txt")
